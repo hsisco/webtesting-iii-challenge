8b6f289a38385dcac045684b5f17699bd1c36957
hsisco <hysensisco@gmail.com>
2019-11-11 17:31:58 -0800

    Display tests + pass

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -13,3 +13,35 @@ test('Gate defaults to unlocked', () => {
 test('Gate defaults to open', () => {
   expect(Display.defaultProps.closed).toBeFalsy();
 });
+
+test('Displays `Locked` when locked, `Closed` when closed and the red-led class', () => {
+  let props = {
+    closed: true,
+    locked: true,
+  };
+  const { container } = render(
+    <Display closed={props.closed} locked={props.locked} />,
+  );
+  const buttons = container.querySelectorAll('.led.red-led');
+  const locked = buttons[0];
+  const closed = buttons[1];
+
+  expect(locked.textContent).toBe('Locked');
+  expect(closed.textContent).toBe('Closed');
+});
+
+test('Displays `Unlocked` when unlocked, `Open` when open and the green-led class', () => {
+  let props = {
+    closed: false,
+    locked: false,
+  };
+  const { container } = render(
+    <Display closed={props.closed} locked={props.locked} />,
+  );
+  const buttons = container.querySelectorAll('.led.green-led');
+  const locked = buttons[0];
+  const closed = buttons[1];
+
+  expect(locked.textContent).toBe('Unlocked');
+  expect(closed.textContent).toBe('Open');
+});
