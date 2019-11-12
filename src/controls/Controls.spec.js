be2aefc4f57e3f1cc7e05d0f3ad9f07a53757c20
hsisco <hysensisco@gmail.com>
2019-11-11 17:31:11 -0800

    Controls and Dashboard render + passed

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,15 @@
-// Test away!
+import React from 'react';
+import Controls from './Controls';
+import { render } from '@testing-library/react';
+
+test('Controls renders correctly', () => {
+  render(<Controls />);
+});
+
+test('Gate cannot be closed if locked', () => {
+    const {getByText} = render(
+        <Controls closed={false} locked={false}/>
+    );
+    const lockButton = getByText(/lock gate/i);
+    expect(lockButton.disabled).toBeTruthy();
+})
